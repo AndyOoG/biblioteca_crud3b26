1d55fb749578186994ab3c02be32e1698dad9eff
AndyOoG <andyy.g2788@gmail.com>
2026-06-17 13:30:11 -0600

    Libro_DAO completo

--- databse/conexion.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-class Conexion:
-
-    @staticmethod
-    def obtener_conexion():
-        return psycopg2.connect(
-        host="localhost", database = "biblioteca_3b26",
-        user = "postgres", password = "Enchiladasverdes2.", port =5432)
--- models/prestamo.py
+++ models/prestamo.py
@@ -1,4 +1,4 @@
-from models.
+from models.libro import libro
 class Prestamo:
 
     #constructor
